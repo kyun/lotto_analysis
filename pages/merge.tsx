ce7c00daa8bf0615ccff0f34ac6df6f0d4c911e5
kyun <wls5563@gmail.com>
2022-09-18 17:07:18 +0900

    parser

--- pages/merge.tsx
+++ pages/merge.tsx
@@ -1,13 +1,26 @@
 import type { NextPage } from "next";
-import out from "./api/out.json";
+import { parse } from "node-html-parser";
 
-const Merge: NextPage = () => {
-  return <pre>{JSON.stringify(out, undefined, "\t")}</pre>;
+const Merge: NextPage = ({ out }: any) => {
+  return <pre>{JSON.stringify(JSON.parse(out), undefined, "\t")}</pre>;
 };
 
 export const getStaticProps = async () => {
+  // const url = `https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=`;
+  // const no = 1033;
+  // const res = await fetch(url + no);
+  // const json = await res.json();
+
+  const myUrl = `https://kyun.github.io/lotto_analysis/merge`;
+  const myRes = await fetch(myUrl);
+  // myRes.text();
+  const myJson = await myRes.text();
+  const root = parse(myJson);
+  const text = root.querySelector("pre")?.text;
   return {
-    props: {},
+    props: {
+      out: text,
+    },
   };
 };
 
